4f188b68f5d20a5e7301419440b390c806339479
Lukkan1970 <patrik.luukkonen@live.com>
2017-05-23 09:55:15 +0200

    Seed fixed

--- Gymbokningv0.1/Migrations/Configuration.cs
+++ Gymbokningv0.1/Migrations/Configuration.cs
@@ -1,5 +1,8 @@
 namespace Gymbokningv0._1.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,29 +17,55 @@ namespace Gymbokningv0._1.Migrations
 
         protected override void Seed(Gymbokningv0._1.Models.ApplicationDbContext context)
         {
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            var roleNames = new[] {"Admin", "Member" };
+
+            foreach (var roleName in roleNames)
+            {
+                if (!context.Roles.Any(r => r.Name == roleName) )
+                {
+                    var role = new IdentityRole { Name = roleName };
+                    var result = roleManager.Create(role);
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(string.Join("\n", result.Errors));
+                    }
+                }
+
+            }
+
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            var emails = new[] { "patrik@gymbokning.se", "admin@gymbokning.se" };//, "editor@gymbokning.se", "root@gymbokning.se" };
+
+            foreach (var email in emails)
+            {
+                if (!context.Users.Any(u => u.UserName == email) )
+                {
+                    var user = new ApplicationUser
+                    {
+                        UserName = email,
+                        Email = email
+                    };
+                    var result = userManager.Create(user, "Password");
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(string.Join("\n", result.Errors));
+                    } 
+                }
+            }
+
+            var adminUser = userManager.FindByName("admin@gymbokning.se");
+            userManager.AddToRole(adminUser.Id, "Admin");
+
+            //var memberUser = userManager.FindByName()
 
 
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
 
 
-            //exempel
-            //var roleStore = new RoleStore<IdentyRole>(context);
-            //var roleManager = new RoleManager<IdentyRole>(roleStore);
-            //foreach (var item in collection)
-            //{
-
-            //}
 
         }
     }
